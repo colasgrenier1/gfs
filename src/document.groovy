67c71b6b4d0ca77fbe5c5bf6d7c3f55acb9c1f07
Nicolas <nicolas.grenier2@mail.mcgill.ca>
2019-02-27 05:41:20 -0500

    first commit

---
+++ src/document.groovy
@@ -0,0 +1,120 @@
+/**
+ *
+ */
+class Document {
+	List content
+	Document () {
+		content = LinkedList()
+	}
+}
+
+
+/**
+ * A generalised section to be used by users (carries no
+ * programming content beyond being used by others).
+ */
+class Section {
+
+}
+
+/**
+ *
+ */
+class Paragraph {
+	List content
+	Paragraph () {
+		content = new ArrayList()
+	}
+}
+
+/**
+ * A line is like a paragraph except it does not allow
+ * multiple ones: if a new paragraph is detected in the
+ * context an error is thrown.
+ */
+class Line {
+
+}
+
+/**
+ * A single word, without punctuation or space.
+ */
+class Word implements TextElement {
+	String content
+	Word (String content) {
+		this.content = content
+	}
+}
+
+/**
+ * Space.
+ */
+class Space implements TextElement {
+	boolean breaking
+	Word (boolean breaking) {
+		this.breaking = breaking
+	}
+	Word () {
+		breaking = false
+	}
+}
+
+/**
+ * Punctuation mark.
+ */
+class Punctuation implements TextElement {
+	String content
+	Punctuation (String content) {
+		this.content = content
+	}
+}
+
+interface TextElement {
+	//I think this will be left empty for now
+}
+
+/**
+ *
+ */
+class Text {
+	List content
+	Text () {
+		content = ArrayList()
+	}
+
+	void add(Object elem) {
+
+	}
+}
+
+/**
+ *
+ */
+class Footnote {
+
+}
+
+/**
+ *
+ */
+class BlockQuote {
+
+}
+
+/**
+ *
+ */
+class Poetry {
+
+}
+
+/**
+ *
+ */
+class URL {
+
+}
+
+interface Element {
+
+}
